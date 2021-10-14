189d6d9540fbfd54a1224e4a133e7270a0fa32e9
kahn430 <tkahn0430@gmail.com>
2021-10-14 11:27:42 +0300

    initial commit

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'd3b75b23bc444f4382b10a597434376c',
-    clientSecret: '651a80a2045d4ee59edcad6b54820149'
+    clientId: '',
+    clientSecret: ''
   }
 }
